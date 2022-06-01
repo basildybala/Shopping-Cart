4a22ba690c433ce6453919bba03ba5bf1864e04d
basildybala <akbasil123@gmail.com>
2022-06-01 14:12:09 +0530

    add product edit product array added deleted

--- Server/Controller/admin.js
+++ Server/Controller/admin.js
@@ -1,7 +1,7 @@
 const Product = require("../models/Product");
 const slugify=require('slugify');
 
-
+//ADMIN HOME PAGE
 exports.homePage=async (req,res)=>{
     try {
         
@@ -10,6 +10,10 @@ exports.homePage=async (req,res)=>{
         
     }
 }
+
+
+//PRODUCTS
+
 exports.allProducts=async (req,res)=>{
     try {
      let products=await Product.find()
@@ -18,24 +22,19 @@ exports.allProducts=async (req,res)=>{
         console.log(error);
     }
 }
-exports.editProduct=async (req,res)=>{
-    try {
-        res.render('admin/edit-product')
-    } catch (error) {
-        
-    }
-}
+//ADD PRODUCTS
+
 exports.addProduct=async (req,res)=>{
-    try {
-        
+    try {  
         res.render('admin/add-product')
     } catch (error) {
-        
+        res.status(500).render('page-not-found')
     }
 }
 exports.addProductPost=async (req,res)=>{
     try {
-        const{title,price,desc,category,qty,instock,categories,size,color}=req.body;
+        
+        const{title,price,desc,category,qty,instock,categories,size,color,listingprice}=req.body;
         if(req.files.length >0){
             let path = ''
             req.files.forEach(function(files,index,arr){
@@ -53,9 +52,11 @@ exports.addProductPost=async (req,res)=>{
             qty,
             instock,
             productPictures,
+            
             categories,
             color,
-            size
+            size,
+            listingprice
         })
 
         product.save().then(r=>{
@@ -65,8 +66,76 @@ exports.addProductPost=async (req,res)=>{
             console.log(e);
         })
         
-        res.render('admin/add-product')
+        res.redirect('/admin/all-products')
     } catch (error) {
-        
+        console.log(error);
+        res.status(500).render('page-not-found')
     }
 }
+
+
+//EDIT PRODUCTS
+
+exports.editProduct=async (req,res)=>{
+    try {
+        let proId=req.params.id
+        let product= await Product.findById(proId)
+        res.render('admin/edit-product',{product})
+    } catch (error) {
+        console.log(error);
+        res.status(500).render('page-not-found')
+    }
+}
+exports.editProductPost=async (req,res)=>{
+    try {
+        let proId=req.params.id
+        if(req.files.length >0){
+            let Pictures =req.body.productPictures
+            let path = Pictures+','
+            req.files.forEach(function(files,index,arr){
+            path=path+files.path+','
+            })
+            path=path.substring(0,path.lastIndexOf(","))
+             let productPictures =path.split(',')
+             let body=req.body
+            let finalBody={...body,productPictures}
+            console.log(finalBody);
+            const updateProdoct = await Product.findByIdAndUpdate(proId,
+                {
+                  $set: finalBody,
+                },
+                { new: true }
+              );
+              res.status(200).redirect('/admin/all-products');
+        }else{
+            const updateProdoct = await Product.findByIdAndUpdate(proId,
+                {
+                  $set: req.body,
+                },
+                { new: true }
+              );
+              res.status(200).redirect('/admin/all-products');
+        }
+        
+       
+
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).render('page-not-found')
+    }
+}
+
+//DELETE PRODUCTS
+
+exports.deleteProduct=async (req,res)=>{
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).redirect("/admin/all-products");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+}
+
+
--- Server/Routes/admin.js
+++ Server/Routes/admin.js
@@ -1,32 +1,45 @@
 const router=require('express').Router()
 const controller=require('../Controller/admin')
+let shortid=require('shortid');
+const path =require('path');
+const multer=require('multer');
+const {
+    verifyToken,
+    verifyTokenAndAdmin,
+  } = require("./verifyToken");
 
-// const multer=require('multer');
 
 
-// const storage= multer.diskStorage({
+const storage= multer.diskStorage({
  
-//   destination:function(req,file,cb){
-//   cb(null,"public/images/uploads/")
-//   },
-//   filename:function(req,file,cb){
-//     let ext= path.extname(file.originalname)
-//   cb(null,shortid.generate()+'-'+file.originalname)
-//   }
-// }) 
-// const upload=multer({storage:storage})
-// router.post("/create", verifyTokenAndAdmin,upload.array('productPictures'),controller.AddProduct);
+  destination:function(req,file,cb){
+   
+  cb(null,"public/images/uploads/")
+  },
+  filename:function(req,file,cb){
+    let ext= path.extname(file.originalname)
+  cb(null,shortid.generate()+'-'+file.originalname)
+  }
+}) 
+const upload=multer({storage:storage})
+
 
 //Admin Home Page Route
 router.get('/',controller.homePage)
 
 //Products Route
 router.get('/all-products',controller.allProducts)
+
+//Add product
 router.get('/add-product',controller.addProduct)
-router.post('/add-product',controller.addProductPost)
+router.post('/add-product',verifyToken,upload.array('productPictures'),controller.addProductPost)
+
+//Edit Product
 router.get('/edit-product/:id',controller.editProduct)
-router.post('/edit-product/:id',controller.editProduct)
-router.post('/delete-product',controller.homePage)
+router.post('/edit-product/:id',upload.array('productPictures'),controller.editProductPost)
+
+//Delete Product
+router.get('/delete-product/:id',controller.deleteProduct)
 
 
 
